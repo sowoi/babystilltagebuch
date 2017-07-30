ceb5e2767b48e5c57468e229930f5d7a058f345d
sowoi <sowinnow@outlook.com>
2017-07-30 22:40:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
         .legend rect {
             fill:white;
             stroke:black;
-            opacity:0.8;}
+            opacity:0.4;}
 
     </style>
 </head>
@@ -41,6 +41,8 @@
 
         <p>Datum:<br>
             <input type="Text" name="datum" value='<?php echo date('d-m-Y');?>'></p>
+        <input type="hidden" name="datec" value="<?php echo date('Ymd');?>" />
+
 
         <p>Zeit:<br>
             <input type="Text" name="zeit" value='<?php echo date('H:i');?>'></p>
@@ -48,14 +50,22 @@
         <p>Menge:<br>
             <input type="Text" name="menge"></p>
 
-        <p>Gewicht:<br>
+        <p>Gewicht in g:<br>
             <input type="Text" name="gewicht"></p>
 
-        <p>Größe:<br>
+        <p>Größe in cm:<br>
             <input type="Text" name="groesse"></p>
 
+        <p>Soll der letzte Wert überschreiben werden?<br>
+            <input type="hidden" name="mengel" value="0" />
+            <input type="checkbox"  name="mengel" value="1"> Menge<br>
+            <input type="hidden" name="gewichtel" value="0" />
+            <input type="checkbox"  name="gewichtel" value="1"> Gewicht<br>
+            <input type="hidden" name="grossel" value="0" />
+            <input type="checkbox"  name="grossel" value="1"> Größe<br> <br><br>
 
-        <input type="Submit" name="" value="speichern">
+
+            <input type="Submit" name="" value="speichern">
 
     </form>
 
@@ -75,38 +85,160 @@ if ( isset($_GET['datum']) <> "" )
     $handle3 = fopen ( "gewicht.txt", "a" );
     $handle4 = fopen ( "groesse.txt", "a" );
     $handle5 = fopen ("visual.txt", "a");
+  //  $handle19 = fopen ("datec.txt", "w+");
+
 
     $data = $_GET['datum'];
-    $datb = date('Ymd');
+    // $datb = $_GET['datec'];
     $nama = $_GET['zeit'];
     $menga = $_GET['menge'];
     $gewichta = $_GET['gewicht'];
     $groessa = $_GET['groesse'];
     $groessb = $_GET['groesse']. PHP_EOL;
 
+    $year = substr($data, 6);
+    $month = substr($data, 3, -5);
+    $day = substr($data, 0, -8);
+
+    $datb = $year.$month.$day;
+
+
 
     $jclgwo = file_get_contents('gewicht.txt');
     $jclgso = file_get_contents('groesse.txt');
+    $jclinput = file_get_contents('jsonval.txt');
     $jclgsjo = (json_decode($jclgso, false));
     $jclgwjo = (json_decode($jclgwo, false));
+    $jclinputo = (json_decode($jclinput, false));
     $gjwicht = array();
     $gjwdato = array();
     $gjsseo = array();
     $gjwdatso = array();
+    $masse = array();
+    $gjmmaa = array();
+    $gjmmza = array();
+    $nulleol = "0". PHP_EOL;
 
-    foreach ($jclgwjo as $topp) {
-        $gjwichto[] = $topp -> Gewicht;
-        $lastG = (int)end($gjwichto);
 
+
+
+    if (is_array($jclgwjo) || is_object($jclgwjo)){
+        foreach ($jclgwjo as $topp) {
+            $gjwichto[] = $topp -> Gewicht;
+            $lastG = (int)end($gjwichto);
+            $gjwdato[] = $topp -> Datum;
+            $lastDo = end($gjwdato);
+            $yeargw = substr($lastDo, 6);
+            $monthgw = substr($lastDo, 3, -5);
+            $daygw = substr($lastDo, 0, -8);
+
+            $lastDoCG = $yeargw.$monthgw.$daygw;
+
+        }
     }
+
 
 
     foreach ($jclgsjo as $toppa) {
         $gjsseo[] = $toppa -> Groesse;
         $lastGs = (int)end($gjsseo);
         $lastGss = $lastGs.PHP_EOL;
+        $gjsseo[] = $toppa -> Datum;
+        $lastDso = end($gjsseo);
+        $yeargs = substr($lastDso, 6);
+        $monthgs = substr($lastDso, 3, -5);
+        $daygs = substr($lastDso, 0, -8);
+
+        $lastDoCGs = $yeargs.$monthgs.$daygs;
 
     }
+
+    foreach ($jclinputo  as $topping) {
+        $masse[] = (int)($topping -> Menge);
+        $lastMDo = (int)end($masse);
+        $gjmmaa[] = $topping -> Datum;
+        $lastMDao = end($gjmmaa);
+        $gjmmza[] = $topping -> Zeit;
+        $lastMDzo = end($gjmmza);
+    }
+
+
+
+
+
+
+
+
+
+    if($_GET['mengel'] == true){
+
+        $file16 = 'json.txt';
+        $mmurrenta = file_get_contents($file16);
+        $phrase = "{\"Datum\":\"$lastMDao\",\"Zeit\":\"$lastMDzo\",\"Menge\":\"$lastMDo\"}";
+        $mmurrentb = str_replace (  "$phrase",  "" , $mmurrenta );
+        $mmurrentc = file_put_contents($file16, $mmurrentb);
+        $file17 = 'input.txt';
+        $miurrenta = file_get_contents($file17);
+        $phrase2 = "$lastMDao|$lastMDzo|$lastMDo";
+        $miurrentb = str_replace (  "$phrase2",  "" , $miurrenta );
+        var_dump($miurrentb);
+        $miurrentc = file_put_contents($file17, $miurrentb);
+
+
+    }
+
+    if($_GET['gewichtel'] == true and $_GET['grossel'] == false ){
+
+        $file15 = 'gewicht.txt';
+        $gsrrenta = file_get_contents($file15);
+        $phrase = ",{\"Datum\":\"$lastDo\",\"Gewicht\":\"$lastG\"}]";
+        $gsrrentb = str_replace (  "$phrase",  "," , $gsrrenta );
+        $gsrrentc = file_put_contents($file15, $gsrrentb);
+        $file17 = 'visual.txt';
+        $miurrenta = file_get_contents($file17);
+        $phrase2 = "$lastDoCGs\t$lastG\t$lastGs";
+        $miurrentb = str_replace (  "$phrase2",  "" , $miurrenta );
+        $miurrentc = file_put_contents($file17, $miurrentb);
+
+    }
+
+
+
+    if($_GET['grossel'] == true and ($_GET['gewichtel'] == false)){
+
+        $file15 = 'groesse.txt';
+        $gsrrenta = file_get_contents($file15);
+        $phrase = ",{\"Datum\":\"$lastDso\",\"Groesse\":\"$lastGs\"}]";
+        $gsrrentb = str_replace (  "$phrase",  "," , $gsrrenta );
+        $gsrrentc = file_put_contents($file15, $gsrrentb);
+        $file17 = 'visual.txt';
+        $miurrenta = file_get_contents($file17);
+        $phrase2 = "$lastDoCGs\t$lastG\t$lastGs";
+        $miurrentb = str_replace (  "$phrase2",  "" , $miurrenta );
+        $miurrentc = file_put_contents($file17, $miurrentb);
+
+
+
+
+    }
+
+    if(($_GET['grossel'] == true) and ($_GET['gewichtel'] == true)){
+
+        $file17 = 'visual.txt';
+        $miurrenta = file_get_contents($file17);
+        $phrase2 = "$lastDoCG\t$lastG\t$lastGs";
+        $miurrentb = str_replace (  "$phrase2",  "" , $miurrenta );
+        //var_dump($miurrentb);
+        $miurrentc = file_put_contents($file17, $miurrentb);
+        var_dump($dateb);
+
+
+
+
+
+    }
+
+
 
 
 
@@ -143,6 +275,8 @@ if ( isset($_GET['datum']) <> "" )
         $url = htmlspecialchars($_SERVER['HTTP_REFERER']);
         echo "<center><a href='$url'>Zurück</a></center>";
 
+
+
     }else{
         if(!empty($menga)){
             fwrite ( $handle, $data);
@@ -156,47 +290,96 @@ if ( isset($_GET['datum']) <> "" )
             $myObj->Menge = $menga;
             $myJSON = json_encode($myObj);
         }
-        if(!empty($gewichta) and !empty($grossa)){
-            fwrite ($handle5, $datb);
-            fwrite ( $handle5, "\t");
-            fwrite ($handle5, $gewichta);
-            fwrite ( $handle5, "\t" );
-            fwrite($handle5, $groessb);
-        }elseif(!empty($_GET['gewicht'])){
+        if(!empty($gewichta) and !empty($groessa)){
             $myObj2->Datum = $data;
             $myObj2->Gewicht = $gewichta;
             $myJSON2 = json_encode($myObj2);
-            fwrite ($handle5, $datb);
-            fwrite ( $handle5, "\t");
-            fwrite ($handle5, $gewichta);
-            if (empty($groessa) and !empty($gewichta)){
-                fwrite ( $handle5, "\t" );
-                fwrite($handle5, $lastGss);
-            }else{
-                fwrite ( $handle5, "\t" );
-                fwrite($handle5, $groessb);
-
-            }
-            fwrite ($handle3, $myJSON2);
-        }elseif(!empty($_GET['groesse'])){
+            fwrite($handle3, $myJSON2);
             $myObj4->Datum = $data;
             $myObj4->Groesse = $groessa;
             $myJSON4 = json_encode($myObj4);
+            fwrite ($handle4, $myJSON4);
             fwrite ($handle5, $datb);
             fwrite ( $handle5, "\t" );
-            if (empty($gewichta)){
-                fwrite($handle5, $lastG);
+            fwrite($handle5, $gewichta);
+            fwrite ( $handle5, "\t" );
+            fwrite($handle5, $groessb);
+            fwrite ( $handle19, $datb);
+            fclose ( $handle19 );
+        }
+        if(!empty($gewichta) and empty($groessa)) {
+            $myObj2->Datum = $data;
+            $myObj2->Gewicht = $gewichta;
+            $myJSON2 = json_encode($myObj2);
+            fwrite($handle3, $myJSON2);
+            fwrite($handle5, $datb);
+            fwrite ( $handle19, $datb);
+            fclose ( $handle19 );
+            fwrite($handle5, "\t");
+            fwrite($handle5, $gewichta);
+            fwrite($handle5, "\t");
+            if (empty($groessa) and !empty($gewichta)) {
+                if(isset($lastGss)){
+                    fwrite($handle5, $lastGss);
+                }else{
+                    fwrite($handle5, "0");
+                }
+
+            } else {
+
+                fwrite($handle5, $groessb);
+            }
+
+        }
+        if(!empty($groessa) and empty($gewichta)){
+            $myObj4->Datum = $data;
+            $myObj4->Groesse = $groessa;
+            $myJSON4 = json_encode($myObj4);
+            fwrite ($handle4, $myJSON4);
+            fwrite ($handle5, $datb);
+            fwrite ( $handle19, $datb);
+            var_dump($dateb);
+            fclose ( $handle19 );
+            fwrite ( $handle5, "\t" );
+            if(empty($gewichta)){
+                if(isset($lastG)){
+                    fwrite($handle5, $lastG);
+                }else{
+                    fwrite($handle5, $nulleol);
+                }
+
             }else{
                 fwrite($handle5, $gewichta);
             }
             fwrite ( $handle5, "\t" );
             fwrite($handle5, $groessb);
-            fwrite ($handle4, $myJSON4);
+        }
+
+        if(!isset($lastG) and !isset($lastGGs) and !isset($lastMDo)){
+
+            $file8 = 'visual.txt';
+            $vurrenta = file_get_contents($file8);
+            $add = "date\tGewicht in g\tGröße in cm";
+            $addeol = $add.PHP_EOL;
+            $vurrentb = $addeol.$vurrenta;
+            file_put_contents($file8, $vurrentb);
+
+
         }
         fwrite ( $handle2, $myJSON);
         header( 'HTTP/1.1 303 See Other' );
         header( "Location: index.php" );
+
+        file_put_contents('visualb.txt',
+            preg_replace(
+                '~[\r\n]+~',
+                "\r\n",
+                trim(file_get_contents('visual.txt'))
+            )
+        );
+
         exit();
+
     }
 
     exit();
@@ -212,6 +395,8 @@ if ( isset($_GET['datum']) <> "" )
     fclose ( $handle3 );
     fclose ( $handle4 );
     fclose ( $handle5 );
+
+
 
     $file = 'json.txt';
     $currentb = file_get_contents($file);
@@ -271,11 +456,7 @@ if ( isset($_GET['datum']) <> "" )
     $lurrenth = str_replace (  "}]{" ,  "},{" , $lurrentg );
     file_put_contents($file3, $lurrenth);
 
-    $file8 = 'visual.txt';
-    $vurrenta = file_get_contents($file8);
-    $add = "[ <br>";
-    $vurrentb = $add.$vurrenta;
-    file_put_contents($file8, $vurrentb);
+
 
 
     exit;
@@ -351,7 +532,7 @@ file_put_contents($file3, $lurrentj);
 <?php
 
 
-echo "<center><strong> Die letzten 5 Einträge </strong> <br />";
+echo "<center><br><strong> Die letzten 5 Einträge </strong> <br />";
 $data = array_slice(file('input.txt'), -5);
 foreach ($data as $line) {
     echo "<pre>$line</pre>";
@@ -366,11 +547,6 @@ $jclgw = file_get_contents('gewicht.txt');
 $jclgs = file_get_contents('groesse.txt');
 $jclgsj = (json_decode($jclgs, false));
 $jclgwj = (json_decode($jclgw, false));
-
-//var_dump($jsod);
-//echo $jsod[1]->Menge;
-//echo $jsod[2]->Menge;
-//echo $jsod[3]->Menge;
 
 $masse = array();
 $tages = array();
@@ -406,29 +582,36 @@ foreach ($jsod as $topping) {
 
 }
 
-foreach ($jclgwj as $topp) {
-    $gjwicht[] = $topp -> Gewicht;
-    $lastG = (int)end($gjwicht);
-    $lastGG = (int)prev($gjwicht);
-    $gjwdat[] = $topp -> Datum;
-    $lastD = end($gjwdat);
-    $lastDD = prev($gjwdat);
+if (is_array($jclgwj) || is_object($jclgwj)){
+    foreach ($jclgwj as $topp) {
+        $gjwicht[] = $topp -> Gewicht;
+        $lastG = (int)end($gjwicht);
+        $lastGG = (int)prev($gjwicht);
+        $gjwdat[] = $topp -> Datum;
+        $lastD = end($gjwdat);
+        $lastDD = prev($gjwdat);
+    }
 }
+
 
 
 foreach ($jclgsj as $toppa) {
     $gjsse[] = $toppa -> Groesse;
     $lastGs = (int)end($gjsse);
     $lastGGs = (int)prev($gjsse);
-    $gjwdats[] = $topp -> Datum;
+    $gjwdats[] = $toppa -> Datum;
     $lastDs = end($gjwdats);
     $lastDDs = prev($gjwdats);
 }
 
 
 $gegenwa = array_sum($tages);
-$vergang = array_sum($tagesj);
-$diffgegve = $gegenwa - $vergang;
+if(isset($tagesj)){
+    $vergang = array_sum($tagesj);
+    $diffgegve = $gegenwa - $vergang;
+}
+
+
 $diffgegvegw = $lastG - $lastGG;
 $diffgegvegs = $lastGs - $lastGGs;
 
@@ -436,7 +619,6 @@ if((isset($tagesj)) and (isset($tagesjj)) and (isset($tagesjjj))){
     $summedreitage = array_sum($tagesj)+array_sum($tagesjj)+array_sum($tagesjjj);
 }
 
-var_dump($lastGG);
 echo "<center><br>Heute getrunken: " . $gegenwa . " ml \n </center>";
 if(isset($tagesj)){
     echo "<center><br>Gestern getrunken: " . $vergang . " ml \n </center>";
@@ -460,14 +642,17 @@ if(isset($diffgegvegw)){
 
 
 ?>
-<script src="http://d3js.org/d3.v3.js"></script>
-<script src="d3.legend.js"></script>
+<br>
+<br>
+<center><strong>Verlauf von Gewicht und Größe im Laufe der Zeit:</strong></center><br>
+<script src="https://d3js.org/d3.v3.js"></script>
+<script src="d3legend.js"></script>
 <script>
 
 
-    var margin = {top: 20, right: 80, bottom: 30, left: 50},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+    var margin = {top: 5, left: 50, right: 50, bottom: 110},
+        width = 950 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
 
     var parseDate = d3.time.format("%Y%m%d").parse;
 
@@ -498,7 +683,7 @@ if(isset($diffgegvegw)){
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    d3.tsv("visual.txt", function(error, data) {
+    d3.tsv("visualb.txt", function(error, data) {
         color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
 
         data.forEach(function(d) {
@@ -558,12 +743,12 @@ if(isset($diffgegvegw)){
         legend = svg.append("g")
             .attr("class","legend")
             .attr("transform","translate(50,30)")
-            .style("font-size","12px")
+            .style("font-size","10px")
             .call(d3.legend)
 
         setTimeout(function() {
             legend
-                .style("font-size","20px")
+                .style("font-size","16px")
                 .attr("data-style-padding",10)
                 .call(d3.legend)
         },1000)
